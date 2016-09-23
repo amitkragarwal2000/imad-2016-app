3d64bcf9ea8a81aadff91556dd5e5fcde69d9655
amitkragarwal2000 <amitkragarwal2000@users.noreply.github.com>
2016-09-23 18:36:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: "Articel One ! Amit",
+    heading:"Article ONE",
+    date:"23rd march",
+    content:`
+    <h1>Hello, world!</h1>
+    <h2>This is the FIRST ARTICLE PAGE</h2>
+    <P>SAMLE taxt goes here. SAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes hereSAMLE taxt goes here</P> 
+    `
+};
+
+function createTemplate(data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate =     `
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    ${title}
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  ${heading}
+  ${date}
+   ${content}
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+    `;
+    return htmlTemplate;
+}
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articelOne));
 });
 
 app.get('/article-two', function (req, res) {
